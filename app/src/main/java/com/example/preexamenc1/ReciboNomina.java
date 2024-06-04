bc7c018acff661a10935d472b74113df643cfd84
Carol García <jake.v.garcia.mazatlan@gmail.com>
2024-06-04 12:43:54 -0700

    Se realizo la clase recibo nomina

---
+++ app/src/main/java/com/example/preexamenc1/ReciboNomina.java
@@ -0,0 +1,48 @@
+package com.example.preexamenc1;
+
+public class ReciboNomina{
+    private int numRecibo;
+    private String nombre;
+    private float horasTrabNormales;
+    private float horasTrabExtras;
+    private int puesto;
+    private float porcentajeImpuesto;
+
+    public ReciboNomina(int numRecibo, String nombre, float horasTrabNormales, float horasTrabExtras, int puesto) {
+        this.numRecibo = numRecibo;
+        this.nombre = nombre;
+        this.horasTrabNormales = horasTrabNormales;
+        this.horasTrabExtras = horasTrabExtras;
+        this.puesto = puesto;
+        this.porcentajeImpuesto = 0.16f; // 16% de impuesto
+    }
+
+    public float calcularPagoBase() {
+        float pagoBase = 200;
+        switch (puesto) {
+            case 1:
+                pagoBase *= 1.20; // Incremento del 20%
+                break;
+            case 2:
+                pagoBase *= 1.50; // Incremento del 50%
+                break;
+            case 3:
+                pagoBase *= 2.00; // Incremento del 100%
+                break;
+        }
+        return pagoBase;
+    }
+
+    public float calcularSubtotal() {
+        float pagoBase = calcularPagoBase();
+        return (pagoBase * horasTrabNormales) + (horasTrabExtras * pagoBase * 2);
+    }
+
+    public float calcularImpuesto() {
+        return calcularSubtotal() * porcentajeImpuesto;
+    }
+
+    public float calcularTotalPagar() {
+        return calcularSubtotal() - calcularImpuesto();
+    }
+}
